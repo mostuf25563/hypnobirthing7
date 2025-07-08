a75209441ccb1e2e80eda3a203fc0d69b8d8dfca
mostuf25563 <125549956+mostuf25563@users.noreply.github.com>
2025-07-08 20:41:39 +0300

    Add Facebook link to footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -35,7 +35,7 @@ export const Footer: React.FC = () => {
               <IconButton color="inherit" component={Link} href="https://www.instagram.com/mariana.shaham/">
                 <Instagram />
               </IconButton>
-              <IconButton color="inherit" component={Link} href="https://facebook.com/mary.shaham">
+              <IconButton color="inherit" component={Link} href="https://www.facebook.com/mary.shaham">
                 <Facebook />
               </IconButton>
               <IconButton color="inherit" component={Link} href="mailto:maryza1982@gmail.com">
